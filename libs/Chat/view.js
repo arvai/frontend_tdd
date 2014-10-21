17f14b1f14663997458d1fa90a062d064c3e7812
Zsolt Árvai <arvaizsolt@gmail.com>
2014-10-21 07:55:34 +0000

    Homework - chat

---
+++ libs/Chat/view.js
@@ -0,0 +1,81 @@
+/**
+ * @module views/Chat
+ */
+
+var Backbone = require('backbone'),
+
+	/**
+	 * @class
+	 * @extends external:Backbone.Marionette.View
+	 */
+		Chat = Backbone.Marionette.View.extend(
+		/** @lends module:views/Chat~Chat.prototype */
+		{
+			/** @var {Array}  Array of messages in the chat window. */
+			messages: [],
+
+			/** @var {Object} Object of ui element selectors */
+			ui : {
+				sendBtn     : '#sendBtn',
+				msgInput    : '#msgInput',
+				messagesCnt : '#messagesContainer'
+			},
+
+			/** @var {Object} Object of event handlers */
+			events : {
+				'click @ui.sendBtn' : 'onBtnClick'
+			},
+			
+			/**
+			 * On Send button click event handler.
+			 */
+			onBtnClick: function() {
+				var message = this.$(this.ui.msgInput).val();
+
+				this.addMessage(message);
+			},
+			
+			/**
+			 * Add a message if it is valid, then call render method.
+			 * @param {String} text Text of the message.
+			 */
+			addMessage: function(text) {
+				if (this.isValid(text)) {
+					this.messages.push(text);
+					this.renderMessages();
+				}
+			},
+			
+			/**
+			 * Checks if the given text is valid or not.
+			 * @param {String} text Text of the message.
+			 * @returns {Boolean}
+			 */
+			isValid: function(text) {
+				if (text.trim().length > 0 && text.trim().length <= 40) {
+					return true;
+				}
+				else {
+					return false;
+				}
+			},
+
+			/**
+			 * Renders the markup of all messages, and appends it into the
+			 * msg container.
+			 */
+			renderMessages: function() {
+				var renderedMessages = '',
+					messagesCnt      = this.$(this.ui.messagesCnt);
+				
+				for (var i = 0; i < this.messages.length; i++) {
+					renderedMessages += '<li>' + this.messages[i] + '</li>';
+				}
+				messagesCnt.html('<ul>' + renderedMessages + '</li>');
+
+				messagesCnt[0].scrollTop = messagesCnt[0].scrollHeight;
+			}
+		}
+	);
+
+module.exports = Chat;
---
+++ tests/Chat/view.js
@@ -0,0 +1,43 @@
+var assert = require('chai').assert,
+	sinon = require('sinon'),
+	Backbone = require('backbone'),
+	testContent = require('./content/testcontent.html'),
+
+	Chat = require('../../libs/Chat/view');
+
+suite('Testing Backbone Chat', function() {
+	setup(function() {
+		Backbone.$('#sandbox').html(testContent);
+
+		sinon.spy(Chat.prototype, 'onBtnClick');
+		sinon.spy(Chat.prototype, 'addMessage');
+
+		this.chat = new Chat({
+			el : '#sandbox'
+		});
+	});
+
+	teardown(function() {
+		Chat.prototype.onBtnClick.restore();
+		Chat.prototype.addMessage.restore();
+
+		this.chat.messages = [];
+	});
+
+	test('Test - if sendBtn triggers its event', function() {
+		Backbone.$(this.chat.ui.sendBtn).trigger('click');
+
+		assert.isTrue(this.chat.onBtnClick.calledOnce);
+	});
+	
+	test('Test - if valid message is stored', function() {
+		var testMessage = 'Hi dude.';
+
+		Backbone.$(this.chat.ui.msgInput).val(testMessage);
+		Backbone.$(this.chat.ui.sendBtn).trigger('click');
+
+		assert.include(this.chat.messages, testMessage);
+	});
+
+	test('Test - message is rendered', function() {});
+});
